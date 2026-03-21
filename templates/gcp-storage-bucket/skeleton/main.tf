4b863f64a642ba76cc95505e8359cf09a2253067
Gopala Krishna Chennu <chennugopalkrishna@gmail.com>
2026-03-21 10:48:18 -0500

    Add GCP Backstage templates for storage bucket and VPC network

---
+++ templates/gcp-storage-bucket/skeleton/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "{{ values.gcpProjectId }}"
+  region  = "{{ values.gcpRegion }}"
+}
+
+resource "google_storage_bucket" "this" {
+  name                        = "{{ values.bucketName }}"
+  location                    = "{{ values.gcpRegion }}"
+  uniform_bucket_level_access = true
+{{#if values.forceDestroy}}
+  force_destroy               = true
+{{else}}
+  force_destroy               = false
+{{/if}}
+}
+
+resource "google_storage_bucket_versioning" "this" {
+  bucket = google_storage_bucket.this.name
+  versioning {
+{{#if values.enableVersioning}}
+    enabled = true
+{{else}}
+    enabled = false
+{{/if}}
+  }
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.this.name
+}
---
+++ templates/gcp-vpc-network/skeleton/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "{{ values.gcpProjectId }}"
+  region  = "{{ values.gcpRegion }}"
+}
+
+resource "google_compute_network" "vpc" {
+  name                    = "{{ values.networkName }}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "{{ values.subnetName }}"
+  ip_cidr_range = "{{ values.subnetCidr }}"
+  region        = "{{ values.gcpRegion }}"
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "${google_compute_network.vpc.name}-allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["{{ values.sourceCidr }}"]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "${google_compute_network.vpc.name}-allow-http"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["{{ values.sourceCidr }}"]
+}
+
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.subnet.name
+}
